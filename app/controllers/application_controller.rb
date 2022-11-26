a0e801d9c0d991ca5da3c2cb94d9643fe6b8ceea
shin4488 <efshinya48@gmail.com>
2022-11-26 20:38:09 +0900

    add: rdoc sample

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+=begin rdoc
+aaa
+bbb
+受注見積画面
+=end
 class ApplicationController < ActionController::Base
+  # just for document comment test
+  # 受注用のメソッド2
+  def test_document
+    return "test"
+  end
 end
